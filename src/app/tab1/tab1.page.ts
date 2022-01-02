95c261cdc8386b06c46bdd781b9cdc7dfd58fdfe
Lukas Panni <lukas.panni@outlook.de>
2022-01-02 10:36:39 +0100

    Add current rank display

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,7 +8,7 @@ import { PlayerService } from '../services/player.service';
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None
 })
 export class Tab1Page {
   public bonus = new Category('Bonus', 35, 35);
@@ -30,16 +30,17 @@ export class Tab1Page {
     return 12 / (this.players.length + 1);
   }
 
-  constructor(
-    private playerService: PlayerService,
-    private gameService: GameService
-  ) {}
+  constructor(private playerService: PlayerService, private gameService: GameService) {}
+
+  public currentPlacement(player: Player): number {
+    const uniquePoints = new Set(this.playerService.players.map((player) => player.totalPoints));
+    const pointsSorted = Array.from(uniquePoints).sort().reverse();
+    return pointsSorted.indexOf(player.totalPoints) + 1;
+  }
 
   public subTotalChange(player: Player): void {
     if (player.subTotal(this.topCategories) >= this.bonusThreshold)
       player.setPoints(this.bonus, this.bonus.fixedPoints);
-     else
-      player.setPoints(this.bonus, 0);
-
+    else player.setPoints(this.bonus, 0);
   }
 }

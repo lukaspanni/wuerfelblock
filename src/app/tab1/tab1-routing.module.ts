2a871e51313d73ec408302af9158e60c37fcf89e
Lukas Panni <lukas.panni@outlook.de>
2022-01-08 10:19:31 +0100

    Add Leave site warning while game is running

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KeepGameActiveGuard } from './keep-game-active.guard';
 import { Tab1Page } from './tab1.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+    canDeactivate: [KeepGameActiveGuard]
   }
 ];
 

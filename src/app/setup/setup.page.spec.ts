e9b445b2d7490da51bae1da24922af9e5f8962b8
Lukas Panni <lukas.panni@outlook.de>
2022-01-24 19:30:30 +0100

    Fix unit test

--- src/app/setup/setup.page.spec.ts
+++ src/app/setup/setup.page.spec.ts
@@ -2,18 +2,22 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
+import { PersistenceService } from '../services/persistence/persistence.service';
 
 import { SetupPage } from './setup.page';
 
 describe('SetupPage', () => {
   let component: SetupPage;
   let fixture: ComponentFixture<SetupPage>;
+  let persistenceSpy: PersistenceService;
 
   beforeEach(
     waitForAsync(() => {
+      persistenceSpy = jasmine.createSpyObj('PersistenceService', ['store', 'retrieve', 'clear']);
       TestBed.configureTestingModule({
         declarations: [SetupPage],
-        imports: [IonicModule.forRoot(), RouterTestingModule, FormsModule]
+        imports: [IonicModule.forRoot(), RouterTestingModule, FormsModule],
+        providers: [{ provide: PersistenceService, useValue: persistenceSpy }]
       }).compileComponents();
 
       fixture = TestBed.createComponent(SetupPage);

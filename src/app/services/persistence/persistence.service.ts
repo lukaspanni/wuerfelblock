cadcc1fcfb3350c6c93765062c704b51b263f382
Lukas Panni <lukas.panni@outlook.de>
2022-01-24 17:02:30 +0100

    Add PersistenceService interface

---
+++ src/app/services/persistence/persistence.service.ts
@@ -0,0 +1,7 @@
+export abstract class PersistenceService {
+  constructor() {}
+
+  public abstract store(key: string, data: string): Promise<void>;
+  public abstract retrieve(key: string): Promise<string>;
+  public abstract clear(): Promise<void>;
+}

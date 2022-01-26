6f4f5a4126b7b7e6201559ba80ab751bfdd4bd1d
Lukas Panni <lukas.panni@outlook.de>
2022-01-26 18:43:45 +0100

    Implement mobile persistence using ionic-storage

--- src/app/services/persistence/mobile-persistence.service.spec.ts
+++ src/app/services/persistence/mobile-persistence.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MobilePersistenceService } from './mobile-persistence.service';
+import { Storage } from '@ionic/storage-angular';
 
 describe('MobilePersistenceService', () => {
   let service: MobilePersistenceService;
+  let storageSpy: Storage;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    storageSpy = jasmine.createSpyObj('Storage', {
+      set: Promise.resolve(),
+      get: Promise.resolve(''),
+      clear: Promise.resolve(),
+      create: Promise.resolve()
+    });
+    TestBed.configureTestingModule({
+      providers: [{ provide: Storage, useValue: storageSpy }]
+    });
     service = TestBed.inject(MobilePersistenceService);
   });
 

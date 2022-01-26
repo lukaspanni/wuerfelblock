2a2965e887530c43e99ae338f50df9aef8fd2060
Lukas Panni <lukas.panni@outlook.de>
2022-01-26 18:43:45 +0100

    Implement mobile persistence using ionic-storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +13,10 @@ import { StartupComponent } from './startup/startup.component';
 
 @NgModule({
   declarations: [AppComponent, StartupComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: PersistenceService, useFactory: persistenceServiceFactory, deps: [Platform] }
+    { provide: PersistenceService, useFactory: persistenceServiceFactory, deps: [Platform, Storage] }
   ],
   bootstrap: [AppComponent]
 })

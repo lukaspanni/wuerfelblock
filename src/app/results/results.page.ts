9bcc13ebec767841e1c0ec34cc5c5a111b3527e7
Lukas Panni <lukas.panni@outlook.de>
2022-01-27 18:04:07 +0100

    Add Resulsts Page

---
+++ src/app/results/results.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './results.page.html',
+  styleUrls: ['./results.page.scss']
+})
+export class ResultsPage {
+  constructor() {}
+}

fac398396d8edfdc7873f2a38f0016ecf36866f9
Lukas Panni <lukas.panni@outlook.de>
2022-01-27 18:04:07 +0100

    Add Resulsts Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { GameModule } from './game/game.module';
+import { ResultsPageModule } from './results/results.module';
 import { SetupCompleteGuard } from './setup/setup-complete.guard';
 import { SetupPageModule } from './setup/setup.module';
 import { StartupComponent } from './startup/startup.component';
@@ -14,6 +15,11 @@ const routes: Routes = [
   {
     path: 'setup',
     loadChildren: (): Promise<SetupPageModule | void> => import('./setup/setup.module').then((m) => m.SetupPageModule)
+  },
+  {
+    path: 'results',
+    loadChildren: (): Promise<ResultsPageModule | void> =>
+      import('./results/results.module').then((m) => m.ResultsPageModule)
   },
   {
     path: 'startup',

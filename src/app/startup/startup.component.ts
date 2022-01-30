2bb5c5c6ce43469aac09a46fb042df07985eb655
Lukas Panni <lukas.panni@outlook.de>
2022-01-30 18:45:40 +0100

    Limit access to results page to cordova

--- src/app/startup/startup.component.ts
+++ src/app/startup/startup.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
 @Component({
   templateUrl: './startup.component.html',
   styleUrls: ['./startup.component.scss']
 })
 export class StartupComponent {
-  constructor(private router: Router) {}
+  public get resultsLinkHidden(): boolean {
+    return !this.platform.is('cordova');
+  }
+
+  constructor(private router: Router, private platform: Platform) {}
 
   public startSetup(): void {
     this.router.navigateByUrl('/setup');

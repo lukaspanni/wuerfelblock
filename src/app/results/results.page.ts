808fbb012209a7195fb6759d3b8c63ba4d7300bb
Lukas Panni <lukas.panni@outlook.de>
2022-01-30 19:04:15 +0100

    Add simple results page

--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersistenceService } from '../services/persistence/persistence.service';
+export const resultsStorageKey = 'Results';
+
+export type GameResult = {
+  date: Date;
+  points: ResultEntry[];
+};
+
+export type ResultEntry = {
+  player: string;
+  points: number;
+};
 
 @Component({
   templateUrl: './results.page.html',
   styleUrls: ['./results.page.scss']
 })
-export class ResultsPage {
-  constructor() {}
+export class ResultsPage implements OnInit {
+  public storedResults: GameResult[] = [];
+
+  constructor(private persistenceService: PersistenceService) {}
+
+  public async ngOnInit(): Promise<void> {
+    this.loadStoredResults();
+  }
+
+  public async loadStoredResults(): Promise<void> {
+    const resultPromises = (await this.persistenceService.keys())
+      .filter((key) => key.startsWith(resultsStorageKey))
+      .map((key) => this.persistenceService.retrieve(key));
+    const results = await Promise.all(resultPromises);
+    results.forEach((result) => this.storedResults.push(JSON.parse(result)));
+  }
 }

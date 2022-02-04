ff7d72cdcd6eb83108a6a5ab91db38c0984819e9
Lukas Panni <lukas.panni@outlook.de>
2022-02-04 19:56:36 +0100

    Add setup complete guard tests

--- src/app/setup/setup-complete.guard.ts
+++ src/app/setup/setup-complete.guard.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PlayerService } from '../services/player.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class SetupCompleteGuard implements CanActivate {
   constructor(private router: Router, private playerService: PlayerService) {}
@@ -19,15 +13,13 @@ export class SetupCompleteGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!environment.production) return true;
     //Only allow to go to game if setup is complete
-    if (!this.playerService.setupComplete)
+    if (!this.playerService.setupComplete) {
       this.router.navigate(['/setup']);
+      return false;
+    }
 
     return true;
   }

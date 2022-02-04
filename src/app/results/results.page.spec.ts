c81c245f5386af7ef1c1796875a3d0c647716f76
Lukas Panni <lukas.panni@outlook.de>
2022-02-04 18:07:09 +0100

    Add icons for amazon app store release

--- src/app/results/results.page.spec.ts
+++ src/app/results/results.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ResultsPage } from './results.page';
@@ -7,16 +8,18 @@ describe('ResultsPage', () => {
   let component: ResultsPage;
   let fixture: ComponentFixture<ResultsPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ResultsPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ResultsPage],
+        imports: [IonicModule.forRoot(), RouterTestingModule]
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ResultsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(ResultsPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();

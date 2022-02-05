05fb8c30d8c6c55d1d1d6a3895f6702f3ecdd62b
Lukas Panni <lukas.panni@outlook.de>
2022-02-05 11:04:03 +0100

    Add game page tests

--- src/app/game/keep-game-active.guard.spec.ts
+++ src/app/game/keep-game-active.guard.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { KeepGameActiveGuard } from './keep-game-active.guard';
+import { CanLeaveGame, KeepGameActiveGuard } from './keep-game-active.guard';
 
 describe('KeepGameActiveGuard', () => {
   let guard: KeepGameActiveGuard;
@@ -12,5 +12,30 @@ describe('KeepGameActiveGuard', () => {
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('canDeactivate should return true if called without component', () => {
+    expect(guard.canDeactivate(undefined, undefined, undefined, undefined)).toBeTrue();
+  });
+
+  it('canDeactivate should return true if component canLeave returns true', () => {
+    const component: CanLeaveGame = { canLeave: () => true };
+    expect(guard.canDeactivate(component, undefined, undefined, undefined)).toBeTrue();
+  });
+
+  it('canDeactivate should show confirm if component canLeave returns false', () => {
+    const component: CanLeaveGame = { canLeave: () => false };
+    const confirmSpy = spyOn(window, 'confirm');
+    guard.canDeactivate(component, undefined, undefined, undefined);
+    expect(confirmSpy).toHaveBeenCalled();
+  });
+
+  it('canDeactivate should return confirm-result', () => {
+    const component: CanLeaveGame = { canLeave: () => false };
+    const confirmSpy = spyOn(window, 'confirm');
+    confirmSpy.and.returnValue(false);
+    expect(guard.canDeactivate(component, undefined, undefined, undefined)).toBeFalse();
+    confirmSpy.and.returnValue(true);
+    expect(guard.canDeactivate(component, undefined, undefined, undefined)).toBeTrue();
   });
 });

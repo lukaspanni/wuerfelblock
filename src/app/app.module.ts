1d03863179c47e8290570dcd8cc8901e61962740
Lukas Panni <lukas.panni@outlook.de>
2022-02-27 17:02:21 +0100

    Add angular PWA package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,19 @@ import { StartupComponent } from './startup/startup.component';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import localeDeExtra from '@angular/common/locales/extra/de';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 registerLocaleData(localeDe, 'de-DE', localeDeExtra);
 
 @NgModule({
   declarations: [AppComponent, StartupComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     { provide: LOCALE_ID, useValue: 'de-DE' },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }

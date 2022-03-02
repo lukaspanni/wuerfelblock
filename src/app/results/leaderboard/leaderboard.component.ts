d4998b9278ab20d3953c673a7fd4fe0f0ae3b1b6
Lukas Panni <lukas.panni@outlook.de>
2022-03-02 16:51:30 +0100

    Results: make leaderboard expandable
    - dont show every entry on load if many entries exist
    
    - fixes #41

--- src/app/results/leaderboard/leaderboard.component.ts
+++ src/app/results/leaderboard/leaderboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ResultEntry } from '../results.page';
 
 export type LeaderboardEntry = ResultEntry & { placement: number };
@@ -9,8 +9,47 @@ export type Leaderboard = LeaderboardEntry[];
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss']
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
   @Input() public leaderboardData: Leaderboard = [];
+  @Input() public initialSize = 3;
+  @Input() public incrementSize = 3;
+
+  private startIndex = 0;
+  private _endIndex = 0;
+
+  public get showDisplayMoreButton(): boolean {
+    return this.endIndex - this.startIndex < this.leaderboardData.length;
+  }
+
+  public get showDisplayLessButton(): boolean {
+    return this.initialSize < this.leaderboardData.length && !this.showDisplayMoreButton;
+  }
+
+  public get leaderboardDisplayData(): Leaderboard {
+    return this.leaderboardData.slice(this.startIndex, this.endIndex);
+  }
+
+  private get endIndex(): number {
+    return this._endIndex;
+  }
+
+  private set endIndex(index: number) {
+    if (index < this.startIndex) return;
+    if (index > this.leaderboardData.length) index = this.leaderboardData.length;
+    this._endIndex = index;
+  }
 
   constructor() {}
+
+  public ngOnInit(): void {
+    this.endIndex = this.initialSize;
+  }
+
+  public displayMore(): void {
+    this.endIndex = this.endIndex + this.incrementSize;
+  }
+
+  public displazInitial(): void {
+    this.endIndex = this.initialSize;
+  }
 }

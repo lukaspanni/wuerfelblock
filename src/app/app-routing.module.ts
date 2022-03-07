e459b8cb729a4f19c8dac3e531cf6274d6e19ffb
Lukas Panni <lukas.panni@outlook.de>
2022-03-07 17:30:14 +0100

    Settings: Add Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,10 @@ const routes: Routes = [
     component: StartupComponent
   },
   {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then((m) => m.SettingsPageModule)
+  },
+  {
     path: '**',
     redirectTo: 'startup',
     pathMatch: 'full'

9cf4838fd2d1c51062cace12a3dcb3c911a5befb
Lukas Panni <lukas.panni@outlook.de>
2022-03-12 11:59:17 +0100

    Implement SettingsPage

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Setting, SettingsService } from './settings.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss']
 })
 export class SettingsPage implements OnInit {
-  constructor() {}
+  private _settings: Setting[] = [];
 
-  public ngOnInit(): void {}
+  public get settings(): Setting[] {
+    return this._settings;
+  }
+
+  public get settingsAvailable(): boolean {
+    return this._settings.length > 0;
+  }
+
+  constructor(private settingsService: SettingsService) {}
+
+  public ngOnInit(): void {
+    this.settingsService.settings.subscribe((settings) => {
+      this._settings = settings;
+    });
+  }
+
+  public async store(): Promise<void> {
+    this.settingsService.storeSettings(this._settings);
+  }
+
+  public async restoreDefaults(): Promise<void> {
+    this.settingsService.restoreDefaults();
+    this._settings = await this.settingsService.settings.toPromise();
+  }
 }
--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
+import { default as defaultSettings } from '../../assets/default-settings.json';
 import { PersistenceService } from '../services/persistence/persistence.service';
 
 export interface Setting {
@@ -13,24 +15,32 @@ export interface Setting {
   providedIn: 'root'
 })
 export class SettingsService {
-  private _settings: Promise<Setting[]>;
+  private _settings: Observable<Setting[]>;
 
-  public get settings(): Promise<Setting[]> {
+  public get settings(): Observable<Setting[]> {
     return this._settings;
   }
 
   constructor(private persistenceService: PersistenceService) {
-    this._settings = this.loadSettings();
+    this.reload();
   }
 
   public async storeSettings(settings: Setting[]): Promise<void> {
     await this.persistenceService.store('settings', JSON.stringify(settings));
   }
 
+  public reload(): void {
+    this._settings = from(this.loadSettings());
+  }
+
+  public restoreDefaults(): void {
+    this._settings = of(defaultSettings as Setting[]);
+  }
+
   private async loadSettings(): Promise<Setting[]> {
     await this.persistenceService.ready;
     const settingsJson = await this.persistenceService.retrieve('settings');
-    if (settingsJson.length === 0) return [];
+    if (settingsJson.length === 0) return defaultSettings as Setting[];
     return JSON.parse(settingsJson);
   }
 }
